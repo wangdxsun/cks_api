450f4df3f4179cbc637ba28717fbf4f3cf77cc8e
maoxiaojun11 <maoxiaojun11@sina.cn>
2018-07-04 16:13:15 +0800

    添加华夏万家定时同步N1和N1M的任务 , wms部分接口修改

--- Application/API/Controller/WmsController.class.php
+++ Application/API/Controller/WmsController.class.php
@@ -3,6 +3,7 @@ namespace API\Controller;
 
 use Think\Controller;
 use Admin\Model\BaseModel;
+use API\Common\Curl;
 use API\Controller\BaseController;
 /**
  * wms
@@ -10,7 +11,7 @@ use API\Controller\BaseController;
  * @author mxj
  *
  */
-class WmsController extends Controller
+class WmsController extends BaseController
 {
     public function getstatus(){
         $params = json_decode(file_get_contents('php://input'), true);
@@ -38,12 +39,41 @@ class WmsController extends Controller
             exit(json_encode(array('status'=>false,'msg'=>'状态无效或者信息不完整，请检查'),JSON_UNESCAPED_UNICODE));
         }
 
-        $res = BaseModel::saveData(['table'=>'relation','where'=>['clearcd'=>$clearcd],'data'=>['status'=>$status]]);
+        $res = BaseModel::saveData(['table'=>'relation','where'=>['clearcd'=>$clearcd],'data'=>['status'=>$status,'update_time'=>date("Y-m-d H:i:s")]]);
         if(!$res){
             exit(json_encode(array('status'=>false,'msg'=>'执行失败'),JSON_UNESCAPED_UNICODE));
         }else{
             exit(json_encode(array('status'=>true,'msg'=>'执行成功'),JSON_UNESCAPED_UNICODE));
         }
+    }
+
+    /*
+     * 定时更新华夏万家N1和N1M推送接口
+     */
+    public function pushN1Code(){
+        $url = 'http://114.141.173.61:6064/api/pushN1Code';
+        $header = array('Content-Type:application/json','Accept:application/json');
+        $data = BaseModel::getDbData(['table'=>'relation','where'=>['im_model'=>array('in',array('N1M','N1')),'hcode_flag'=>0,'status'=>1]]);
+        $sign = md5(count($data).'phicomm*123');
+        $kcodes = array();
+
+        for($i = 0;$i<count($data);$i++){
+            $kcodes[$i]['hx_code'] = $data[$i]['clearcd'];
+            $kcodes[$i]['hidden_code'] = $data[$i]['hcode'];
+            $kcodes[$i]['nassn'] = $data[$i]['sn'];
+            $kcodes[$i]['order_no'] = $data[$i]['orderid'];
+            $kcodes[$i]['kcode_type'] = $data[$i]['im_model'];
+        }
+
+        $postData['sign'] = $sign;
+        $postData['kcodes'] = $kcodes;
+        $res = json_decode(Curl::curl_header_post($url,json_encode($postData),$header),true);
+
+        if($res['result'] == 200){
+            //按照明码将本次查询到的数据的同步状态改为1
+            BaseModel::saveData(['table'=>'relation','where'=>['clearcd'=>array('in',array_column($data,'clearcd'))],'data'=>['hcode_flag'=>1]]);
+        }
+
     }
 }
 
--- Application/Admin/Controller/WmsController.class.php
+++ Application/Admin/Controller/WmsController.class.php
@@ -27,7 +27,7 @@ class WmsController extends BaseController
         $data = array();
         $res = BaseModel::getDbData(
             ['table'=>'relation',
-                'where'=>['clearcd'=>array('in',array_column($clearcd,'cardId'))],
+             'where'=>['clearcd'=>array('in',array_column($clearcd,'cardId'))],
             ],true);
 
         //查询是否有不存在的k码，将不存在的k码去除
@@ -144,6 +144,7 @@ class WmsController extends BaseController
         $info['readdress'] = $params['readdress'];
         $info['factoryid'] = $params['factoryId'];
         $info['channel2'] = $params['supply'];
+        $info['update_time'] = date("Y-m-d H:i:s");
 
         foreach($info as $k=>$v){
             if(empty($v)){
@@ -155,26 +156,27 @@ class WmsController extends BaseController
 
 
 
-    //数据导入
+    //数据导入(已停止使用)
     public function import()
     {
+
         if(!empty($_FILES['excelFile']['tmp_name']))
         {
             $filename = $_FILES['excelFile']['name'];
             $tmp_name = $_FILES['excelFile']['tmp_name'];
             $extend=strrchr ($filename,'.');
             $extendLower = strtolower($extend);
-
             /*判别是不是.xls和.xlsx文件，判别是不是excel文件*/
             if (($extendLower != ".xls") && ($extendLower != ".xlsx"))
                 exit(json_encode(array('result'=>1, 'msg'=>'请上传excel文件'),JSON_UNESCAPED_UNICODE));
             else{
 
                 //存入excel
+                set_time_limit(0);
+                ini_set("memory_limit","1024M");
                 $file = $this->initExcel($filename,$tmp_name);
                 if(!$file)
                     exit(json_encode(array('result'=>1, 'msg'=>'文件上传失败'),JSON_UNESCAPED_UNICODE));
-                set_time_limit(0);
                 $res = $this->readExcel($file); //读取excel，将excel数据转换为键值对的数组
                 $count = count($res);
 
@@ -186,11 +188,11 @@ class WmsController extends BaseController
                     $res = BaseModel::addalldata(['table'=>'relation', 'data'=>$data]);
                     if(!$res){
                         $M->rollback();
-                        exit(json_encode(array('result'=>1, 'msg'=>'插入失败，请检查表格'),JSON_UNESCAPED_UNICODE));
+                        exit(json_encode(array('result'=>1, 'msg'=>'插入失败，请检查表格'.$i),JSON_UNESCAPED_UNICODE));
                     }
+                    sleep(1);
                 }
                 $M->commit();
-                set_time_limit(30);
                 exit(json_encode(array('result'=>0, 'msg'=>'插入成功，一共插入'.$count.'条数据'),JSON_UNESCAPED_UNICODE));
             }
         }
@@ -241,24 +243,20 @@ class WmsController extends BaseController
             for ($col = 'A'; $col <= $highestColumn; $col++) {
                 $address = $col.$row;
                 $val = $objWorksheet-> getCell($address)-> getValue();
-                if ($col == 'E') {//指定g列为时间所在列 第5列
-                    $strs[$col] = gmdate("Y-m-d H:i:s", \PHPExcel_Shared_Date::ExcelToPHP($val));
-                } else {
-                    $strs[$col] = $val;
-                }
-
+                $strs[$col] = $val;
             }
             //$mylist[$row]['id'] = $strs[0];
             $mylist[$row]['clearcd'] = $strs['B'];
             $mylist[$row]['secretcd'] = $strs['C'];
-            $mylist[$row]['status'] = $strs['D'];
-            $mylist[$row]['im_time'] = $strs['E'];
+            $mylist[$row]['im_pnumber'] = $strs['D'];
+            $mylist[$row]['im_model'] = $strs['E'];
             $mylist[$row]['pmoney'] = $strs['F'];
             $mylist[$row]['close_time'] = $strs['G'];
-            $mylist[$row]['channel1'] = $strs['H'];
+            $mylist[$row]['hcode'] = "";
+            $mylist[$row]['channel1'] = "";
             //$mylist[$row]['allot_time'] = "";
-            $mylist[$row]['pnumber'] = "";
-            $mylist[$row]['pname'] = "";
+            $mylist[$row]['status'] = 0;
+            $mylist[$row]['im_time'] = date("Y-m-d H:i:s");
             $mylist[$row]['jobnu'] = "";
             $mylist[$row]['name'] = "";
             $mylist[$row]['sn'] = "";
@@ -271,12 +269,16 @@ class WmsController extends BaseController
             $mylist[$row]['imei2'] = "";
             $mylist[$row]['mac'] = "";
             $mylist[$row]['factoryid'] = "";
-            $mylist[$row]['im_model'] = "";
-            $mylist[$row]['im_pnumber'] = "";
             $mylist[$row]['im_staff'] = "";
+            $mylist[$row]['pnumber'] = "";
+            $mylist[$row]['pname'] = "";
             $mylist[$row]['channel2'] = "";
+            $mylist[$row]['last_return_time'] = "";
+            $mylist[$row]['hcode_flag'] = 0;
+            $mylist[$row]['update_time'] = date("Y-m-d H:i:s");
             //$mylist[$row]['money'] = "";
         }
+
         return $mylist;
     }
 
