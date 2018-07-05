bc0c1834db3a39b1c57d846c84bc081784b7cf94
maoxiaojun11 <maoxiaojun11@sina.cn>
2018-07-05 11:10:30 +0800

    因为crm需求重新设计k码查询接口

--- Application/API/Controller/WmsController.class.php
+++ Application/API/Controller/WmsController.class.php
@@ -4,47 +4,30 @@ namespace API\Controller;
 use Think\Controller;
 use Admin\Model\BaseModel;
 use API\Common\Curl;
-use API\Controller\BaseController;
 /**
  * wms
  *
  * @author mxj
  *
  */
-class WmsController extends BaseController
+class WmsController extends Controller
 {
     public function getstatus(){
         $params = json_decode(file_get_contents('php://input'), true);
         $clearcd = $params['clearcd'];
 
         $data = BaseModel::getDbData(['table'=>'relation','where'=>['clearcd'=>$clearcd]],false);
-        if(!$data){
-            exit(json_encode(array('status'=>false,'msg'=>$clearcd.': k码不存在'),JSON_UNESCAPED_UNICODE));
-        }else{
-            exit(json_encode(array('status'=>true,'kstatus'=>intval($data['status'])),JSON_UNESCAPED_UNICODE));
-        }
 
-    }
+        $info = array();
+        $info['status'] = $data?true:false;
+        $info['kstatus'] = $data?intval($data['status']):NULL;
+        $info['isExpire'] = $data?((date("Y-m-d H:i:s")>$data['last_return_time'])?0:1):NULL;//如果没查到记录，是否过期返回NULL
+        $info['activeTime'] = $data['allot_time'];
+        $info['msg'] = $data?NULL:"没有查到k码";
 
-    /*
-     *  wms更新k码状态
-     *  author: mxj
-     */
-    public function wchangekcode(){
-        $params = json_decode(file_get_contents('php://input'), true);
-        $clearcd = $params['clearcd'];
-        $status = in_array($params['status'],array(1,3,4))?$params['status']:0; //只能操作1,3,4三个状态，其他状态无效
+        exit(json_encode($info,JSON_UNESCAPED_UNICODE));
 
-        if(empty($clearcd)||empty($status)){
-            exit(json_encode(array('status'=>false,'msg'=>'状态无效或者信息不完整，请检查'),JSON_UNESCAPED_UNICODE));
-        }
 
-        $res = BaseModel::saveData(['table'=>'relation','where'=>['clearcd'=>$clearcd],'data'=>['status'=>$status,'update_time'=>date("Y-m-d H:i:s")]]);
-        if(!$res){
-            exit(json_encode(array('status'=>false,'msg'=>'执行失败'),JSON_UNESCAPED_UNICODE));
-        }else{
-            exit(json_encode(array('status'=>true,'msg'=>'执行成功'),JSON_UNESCAPED_UNICODE));
-        }
     }
 
     /*
@@ -54,6 +37,8 @@ class WmsController extends BaseController
         $url = 'http://114.141.173.61:6064/api/pushN1Code';
         $header = array('Content-Type:application/json','Accept:application/json');
         $data = BaseModel::getDbData(['table'=>'relation','where'=>['im_model'=>array('in',array('N1M','N1')),'hcode_flag'=>0,'status'=>1]]);
+        if(empty($data))
+            exit;
         $sign = md5(count($data).'phicomm*123');
         $kcodes = array();
 
@@ -73,6 +58,7 @@ class WmsController extends BaseController
             //按照明码将本次查询到的数据的同步状态改为1
             BaseModel::saveData(['table'=>'relation','where'=>['clearcd'=>array('in',array_column($data,'clearcd'))],'data'=>['hcode_flag'=>1]]);
         }
+        exit;
 
     }
 }
