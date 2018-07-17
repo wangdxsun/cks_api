d8b71066875c2fc9fcc0df92624bb61c10e64488
unknown <zhiwei.yao@PC-20170712GARR.freecomm-networks.com>
2018-07-17 10:13:44 +0800

    submit yzw

--- ddw.php
+++ ddw.php
@@ -6,13 +6,13 @@ $_GET['a']='getrate';
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',false);
+define('APP_DEBUG',true);
 define('BUILD_DIR_SECURE', false);
 define('TMPL_CACHE_ON',false);
 define('DB_FIELD_CACHE',false);
 define('HTML_CACHE_ON',false);
 // 定义应用目录
-define('BIND_MODULE', 'Home');
+define('BIND_MODULE', 'API');
 define('APP_PATH' ,'./Application/');
 define('SITE_PATH',__DIR__);
 define('BASE_PATH',str_replace('\\','/',realpath(dirname(__FILE__).'/'))."/");
